3fac34352aab582fe8c4430385872811adb89f55
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2024-11-25 19:13:14 +0100

    Add enabled request

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 const LOCALHOST = process.env.LOCALHOST;
 const PORT = process.env.PORT;
 
+let isEnabled = true;
+
+app.get('/state', (req, res) => {
+    const enabled = req.query.enabled === 'true';
+    
+    isEnabled = enabled;
+    
+    res.json({ enabled });
+});
+
 app.get('/hello', (req, res) => {
-    res.json({ content: "Hello, world!" });
+    res.json({ content: isEnabled ? "Hello, world!" : "" });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en ${LOCALHOST}:${PORT}`);
+    console.log(`Server running on ${LOCALHOST}:${PORT}`);
 });
